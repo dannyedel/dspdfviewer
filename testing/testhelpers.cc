0a5a3de7650df2f25bfde88e1898a04b1a08dc72
Danny Edel <mail@danny-edel.de>
2015-09-03 16:27:48 +0200

    remove "static" attribute

--- testing/testhelpers.cc
+++ testing/testhelpers.cc
@@ -3,8 +3,8 @@
 #include <QDir>
 
 QString TestHelpers::pdfFilename() {
-	static const QString relativePart = "colored-rectangles.pdf";
-	static const QDir myDir = QDir::current();
+	const QString relativePart = "colored-rectangles.pdf";
+	const QDir myDir = QDir::current();
 	if ( ! myDir.exists(relativePart) ) {
 		throw std::runtime_error("Could not find the test pdf. This is bad.");
 	}
