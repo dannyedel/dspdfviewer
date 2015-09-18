53a64db31fd74a386953762b1f54dab42ef2266b
Benjamin Desef <projekter-git@yahoo.de>
2015-09-18 11:48:38 +0200

    Qt5 for Windows

--- poppler-qt.h
+++ poppler-qt.h
@@ -2,4 +2,13 @@
 #include <poppler-qt4.h>
 #else
 #include <poppler-qt5.h>
+
+#ifndef qtplugin
+  #define qtplugin
+  #if defined(WIN32) || defined(WIN64)
+    #include <QtPlugin>
+    Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+  #endif
+#endif
+
 #endif
