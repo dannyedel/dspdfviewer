835e0207264ed926f7985842354512a96e89d3fd
Danny Edel <mail@danny-edel.de>
2015-11-21 10:08:37 +0100

    Use left half for thumbnail

--- pdfviewerwindow.cpp
+++ pdfviewerwindow.cpp
@@ -302,7 +302,7 @@ void PDFViewerWindow::renderedPageIncoming(QSharedPointer< RenderedPage > render
 
   // It might be a thumbnail. If we're waiting for one, check if it would fit.
   if ( isInformationLineVisible()
-    && renderedPage->getPart() == PagePart::FullPage
+    && renderedPage->getPart() == PagePart::LeftHalf
     && renderedPage->getIdentifier().requestedPageSize() == this->getPreviewImageSize() ) {
     this->addThumbnail(renderedPage->getPageNumber(), renderedPage->getImage());
   }
