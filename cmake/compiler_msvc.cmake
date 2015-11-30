ecb2c24c1208323cf774e76323b7e5a1a7f0be9c
Benjamin Desef <projekter-git@yahoo.de>
2015-11-30 20:01:52 +0100

    Removed MSVC conditional

--- cmake/compiler_msvc.cmake
+++ cmake/compiler_msvc.cmake
@@ -1,13 +1,11 @@
 # MSVC
 
-if(MSVC)
-	option(CompileMTMode "Build in MT mode" ON)
-	if(CompileMTMode)
-		foreach(var CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
-			CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
-			if(${var} MATCHES "/MD")
-				string(REGEX REPLACE "/MD" "/MT" ${var} "${${var}}")
-			endif()
-		endforeach()
-	endif()
+option(CompileMTMode "Build in MT mode" ON)
+if(CompileMTMode)
+	foreach(var CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+		CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+		if(${var} MATCHES "/MD")
+			string(REGEX REPLACE "/MD" "/MT" ${var} "${${var}}")
+		endif()
+	endforeach()
 endif()
