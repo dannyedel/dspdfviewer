654836ec950a92ae567a31d8c7042111d8766973
Benjamin Desef <projekter-git@yahoo.de>
2015-11-30 08:38:48 +0100

    MSVC compiler switch and /MD-/MT replacement options

---
+++ cmake/compiler_msvc.cmake
@@ -0,0 +1,13 @@
+# MSVC
+
+if(MSVC)
+	option(CompileMTMode "Build in MT mode" ON)
+	if(CompileMTMode)
+		foreach(var CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+			CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+			if(${var} MATCHES "/MD")
+				string(REGEX REPLACE "/MD" "/MT" ${var} "${${var}}")
+			endif()
+		endforeach()
+	endif()
+endif()
