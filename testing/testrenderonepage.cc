1dca6e05c72ac844404a8e18f837da3692d34067
Danny Edel <mail@danny-edel.de>
2016-01-15 16:22:48 +0100

    test render one page: Allow for white note page
    On older latex-beamer installations, the note page will always be
    rendered as white, ignoring the note page color.  Since this has broken
    unittests on quite a few still-recent platforms while nothing is
    technically wrong with dspdfviewer, add support for this as a fallback
    color.

--- testing/testrenderonepage.cc
+++ testing/testrenderonepage.cc
@@ -6,22 +6,38 @@ using namespace TestHelpers;
 BOOST_AUTO_TEST_CASE(render_one_page) {
 	PDFDocumentReference pdr( TestHelpers::pdfFilename("colored-rectangles.pdf"), PDFCacheOption::keepPDFinMemory );
 
-	auto left = RenderUtils::renderPagePart(pdr.page(0).page, QSize(1920,1080), PagePart::LeftHalf);
-	auto right = RenderUtils::renderPagePart(pdr.page(0).page, QSize(1920,1200), PagePart::RightHalf);
-	auto both = RenderUtils::renderPagePart(pdr.page(0).page, QSize(3840,1080), PagePart::FullPage);
+	const auto left = RenderUtils::renderPagePart(pdr.page(0).page, QSize(1920,1080), PagePart::LeftHalf);
+	const auto right = RenderUtils::renderPagePart(pdr.page(0).page, QSize(1920,1200), PagePart::RightHalf);
+	const auto both = RenderUtils::renderPagePart(pdr.page(0).page, QSize(3840,1080), PagePart::FullPage);
 
-	auto leftScreenColor = QColor( 0x88, 0xff, 0x88);
+	const auto leftScreenColor = QColor( 0x88, 0xff, 0x88);
 	auto rightScreenColor = QColor( 0xff, 0x88, 0xff);
+
+	/** Older texlive installation: Note page gets rendered white
+	 */
+	const auto fallbackRightScreenColor = QColor( 0xff, 0xff, 0xff);
+
+	BOOST_CHECK_NE(rightScreenColor, fallbackRightScreenColor);
 
 	/** Check sizes of rendered images **/
 	BOOST_CHECK_EQUAL( QSize(1920,1080), left.size());
 	BOOST_CHECK_EQUAL( QSize(1920,1080), right.size());
 	BOOST_CHECK_EQUAL( QSize(3840,1080), both.size());
 
-	/** Check pixels in the middle */
+	/** Check middle pixel of left screen **/
 	BOOST_CHECK_EQUAL( leftScreenColor, QColor(left.pixel(960,540)));
-	BOOST_CHECK_EQUAL( rightScreenColor, QColor(right.pixel(960,540)));
 	BOOST_CHECK_EQUAL( leftScreenColor, QColor(both.pixel(960,540)));
+
+	const auto rightScreenMiddle = QColor(right.pixel(960,540));
+
+	BOOST_WARN_EQUAL( rightScreenColor, rightScreenMiddle );
+	/** Check middle pixel of right screen **/
+	if ( rightScreenMiddle == fallbackRightScreenColor) {
+		BOOST_TEST_MESSAGE( "Middle of note screen was peak white, "
+			", using workaround for older latex-beamer versions.");
+		rightScreenColor = fallbackRightScreenColor;
+	}
+	BOOST_CHECK_EQUAL( rightScreenColor, QColor(right.pixel(960,540)));
 	BOOST_CHECK_EQUAL( rightScreenColor, QColor(both.pixel(2880,540)));
 
 	/** Check all-the-way-left and all-the-way-right pixel colors */
